5d4413f5e2347030bf8ca9a5fbf231cb5efb4a98
LingXi <LingXi@gmail.com>
2019-05-28 20:43:45 +0800

    First commit

---
+++ app/src/main/java/com/mint/ximalaya/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mint.ximalaya;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
