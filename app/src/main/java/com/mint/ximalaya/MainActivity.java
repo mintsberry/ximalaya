52587408d4fff6ce592974af61af5a59b40ad07e
LingXi <LingXi@gmail.com>
2019-05-29 10:10:08 +0800

    add Sdk.

--- app/src/main/java/com/mint/ximalaya/MainActivity.java
+++ app/src/main/java/com/mint/ximalaya/MainActivity.java
@@ -2,12 +2,41 @@ package com.mint.ximalaya;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.ximalaya.ting.android.opensdk.datatrasfer.CommonRequest;
+import com.ximalaya.ting.android.opensdk.datatrasfer.IDataCallBack;
+import com.ximalaya.ting.android.opensdk.model.category.Category;
+import com.ximalaya.ting.android.opensdk.model.category.CategoryList;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Map<String, String> map = new HashMap<String, String>();
+        CommonRequest.getCategories(map, new IDataCallBack<CategoryList>() {
+            @Override
+            public void onSuccess(CategoryList object) {
+                List<Category> categories = object.getCategories();
+                if (categories != null){
+                    int size = categories.size();
+                    Log.d(TAG, "size: " + size);
+                }
+                for (Category category : categories) {
+                    Log.d(TAG, "category: " + category.getCategoryName());
+                }
+            }
+
+            @Override
+            public void onError(int code, String message) {
+                Log.e(TAG, "onError: "+ code + " " + message );
+            }
+        });
     }
 }
